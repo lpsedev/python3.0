16176de2b1d44301d50c05db197dc073e1cc11ac
lpsedev <lpsedeveloper@hotmail.com>
2020-07-10 14:22:08 -0300

    modificado
    Signed-off-by: lpsedev <lpsedeveloper@hotmail.com>

--- ExercicioPython/listas_parte02/desafio085.py
+++ ExercicioPython/listas_parte02/desafio085.py
@@ -3,19 +3,36 @@ Crie um programa onde o usuário possa digitar sete valores numéricos
 e cadastre-os em uma lista única que mantenha separados os valores pares
 e ímpares. No final mostre os valoes pares e ímpares em ordem crescente.
 """
+# 1ª forma da fazer
+# par = []
+# impar = []
+# lista = []
+#
+# for n in range(0 ,7):
+#     num = int(input('Digite um valor: '))
+#     if num % 2 == 0:
+#         par.append(num)
+#         par.sort()
+#     else:
+#         impar.append(num)
+#         impar.sort()
+# lista.append(par[:])
+# lista.append(impar[:])
+# print(f'Os números pares digitados foram {lista[0]}')
+# print(f'Os números ímpares digitados foram {lista[1]}')
 
-numeros = []
-par = []
-impar = []
-for c in range(0, 7):
-    numeros.append(int(input('Digite um valor: ')))
-for p in numeros:
-    if p % 2 == 0:
-        par.append(p)
-        par.sort()
+# 2ª forma de fazer
+
+num = [[], []]
+valor = 0
+for c in range(1, 8):
+    valor = int(input(f'Digite o {c}º número: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+        num[0].sort()
     else:
-        impar.append(p)
-        impar.sort()
-print('=-=' * 25)
-print(f'Esse foram os números pares {par}')
-print(f'Esse foram os números impares {impar}')
+        num[1].append(valor)
+        num[1].sort()
+print('=-=' * 40)
+print(f'Os valores pares digitados foram {num[0]}')
+print(f'Os valores impares digitados foram {num[1]}')
--- ExercicioPython/listas_parte02/desafio086.py
+++ ExercicioPython/listas_parte02/desafio086.py
@@ -6,3 +6,11 @@ No final mostre a matriz na tela, com a formatação
 correta.
 """
 
+matriz = [[0,0,0], [0,0,0], [0,0,0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor {l}, {c}: '))
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()  # quebra de linha
